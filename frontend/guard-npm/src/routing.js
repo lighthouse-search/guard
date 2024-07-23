2bbfcd1b0de813420ba91e7934287eeece0cc302
Josh <91714073+oracularhades@users.noreply.github.com>
2024-07-24 10:15:53 +1200

    Update routing.js

--- frontend/guard-npm/src/routing.js
+++ frontend/guard-npm/src/routing.js
@@ -1,11 +1,11 @@
 function getGuardApiURL() {
     let url = new URL(window.location.href);
     url.port = window.location.port;
-    url.pathname = "/api";
+    url.pathname = "/guard/api";
 
     if (window.location.hostname == "localhost" || window.location.hostname == "127.0.0.1") {
         if (typeof localStorage != "undefined") {
-            const custom_api_endpoint = localStorage.getItem("custom_api_endpoint");
+            const custom_api_endpoint = localStorage.getItem("custom_guard_api_endpoint");
             if (custom_api_endpoint) {
                 url.href = custom_api_endpoint;
             }

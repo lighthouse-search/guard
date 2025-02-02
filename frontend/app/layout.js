63bb327e7e4296540b59be031627de8a37c81298
Josh <hi@oracularhades.com>
2025-02-02 20:51:29 +1300

    Fix guard font

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,9 @@
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
 
 export const metadata = {
 }
@@ -8,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
--- frontend/src/components/base.js
+++ frontend/src/components/base.js
@@ -1,12 +1,15 @@
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import './global.css';
 import '../../styles/global.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
 
 export default function Base(props) {
     return (
-        <div className={`${props.className} ${inter.className}`} style={props.style}>
+        <div className={`${props.className} ${roboto.className}`} style={props.style}>
             {props.metadata && props.metadata.motd_banner && <div className='motd_banner'>
                 <p className='motd_banner_text'>{props.metadata.motd_banner}</p>
             </div>}
